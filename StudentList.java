62d91890aaab00bdf73e339197755c51f74dc609
Shoaib Khan <mdshoaib.a.khan@gmail.com>
2022-11-25 21:27:02 +0600

    Updated main

--- StudentList.java
+++ StudentList.java
@@ -24,56 +24,6 @@ public class StudentList {
 
 						} 
 						System.out.println("Data Loaded.");
-<<<<<<< HEAD
-				}
-				else if(args[0].equals("r")) {
-						System.out.println("Loading data ...");			
-						try {
-								BufferedReader s = new BufferedReader(
-										new InputStreamReader(
-												new FileInputStream("students.txt"))); 
-								String r = s.readLine(); 
-								String i[] = r.split(",");	
-								Random x = new Random();
-								int y = x.nextInt(i.length);
-								System.out.println(i[y].trim());
-						} catch (Exception e){
-
-						} 
-						System.out.println("Data Loaded.");			
-				}
-				else if(args[0].contains("+")){
-						System.out.println("Loading data ...");			
-						try {
-								BufferedWriter s = new BufferedWriter(
-										new FileWriter("students.txt", true));
-								String t = args[0].substring(1);
-	        					Date d = new Date();
-	        					String df = "yyyy-mm-dd hh:mm:ss a";
-	        					DateFormat dateFormat = new SimpleDateFormat(df);
-	        String fd= dateFormat.format(d);
-			s.write(", "+t+"\nList last updated on "+fd);
-			s.close();
-			} catch (Exception e){}							
-			System.out.println("Data Loaded.");	
-		}
-		else if(args[0].contains("?")) 
-		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");	
-			boolean done = false;
-			String t = args[0].substring(1);
-			for(int idx = 0; idx<i.length && !done; idx++) {
-				if(i[idx].equals(t)) {
-					System.out.println("We found it!");
-						done=true;
-=======
->>>>>>> Step#1
 				}
 				else if(args[0].equals("r")) {
 						System.out.println("Loading data ...");			
