1a00f4266cb11399e7cd066caf086d083d74291d
Shoaib Khan <mdshoaib.a.khan@gmail.com>
2022-11-29 23:35:30 +0600

    Constants.java class added

--- Constants.java
+++ Constants.java
@@ -2,8 +2,18 @@ public class Constants {
     public static String improper_arguement = "Please input proper arguement!";
     public static String arguement_a = "a";
     public static String arguement_r = "r";
+    public static String arguement_ques = "?";
+    public static String arguement_c = "c";
     public static String loading = "Loading data ...";
     public static String data_loaded = "Data Loaded.";
-    public static String comma = ","
+    public static String comma = ",";
+    public static String arguement_plus = "+";
+    public static String date_format = "yyyy-mm-dd hh:mm:ss a";
+    public static String word_found = "We found it!";
+    public static String filename = "students.txt";
+    public static String list_Update = "\nList last updated on ";
+    public static String comma_space = ", ";
+    public static String words_found = " word(s) found ";
+
     
 }
